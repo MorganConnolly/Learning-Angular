5f43f56db0d5ba772951ba9643c5ce327b5f12b5
Morgan-Bluefuse <121067624+MorganConnolly@users.noreply.github.com>
2025-10-28 16:37:07 +0000

    Set up profiles: signals, passing values, state, etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { User } from './user/user';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, User],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+
+export class AppComponent {
+  users = DUMMY_USERS;
+}
---
+++ src/app/user/user.ts
@@ -0,0 +1,22 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.html',
+  styleUrl: './user.css',
+})
+
+export class User {
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter();
+
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
+  }
+
+  public onUserClick() {
+    this.select.emit(this.id);
+  }
+}
